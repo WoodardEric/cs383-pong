b875e5a69dbbabf3bd829bbd5ce493c0b8176f0b
WoodyEric <woodard.eric09@gmail.com>
2022-01-21 22:57:37 -0800

    add fonts and fix ResetMatch

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,14 @@ public class GameManager : MonoBehaviour
 {
     public static int PlayerOneScore = 0;
     public static int PlayerTwoScore = 0;
-    private const int MAX_SCORE = 10;
+    private const int MAX_SCORE = 3;
     public Canvas helpScreen;
     public GUISkin layout;
 
     public GameObject ball;
     public AudioSource audioSource;
+
+    private bool restarting = false;
 
     void Start()
     {
@@ -40,20 +42,32 @@ public class GameManager : MonoBehaviour
         GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerOneScore);
         GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerTwoScore);
 
-        var messageRect = new Rect(Screen.width / 2 - 150, Screen.height - 100, 2000, 1000);
+        var messageRect = new Rect(Screen.width / 2 - 250, Screen.height - 100, 2000, 1000);
         bool win = false;
         if (PlayerOneScore >= MAX_SCORE)
         {
             GUI.Label(messageRect, "PLAYER ONE WINS");
-            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
             win = true;
         }
         else if (PlayerTwoScore >= MAX_SCORE)
         {
             GUI.Label(messageRect, "PLAYER TWO WINS");
-            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
             win = true;
         }
+
+        if (win && !restarting)
+        {
+            Invoke("ResetMatch", 5);
+            restarting = true;
+        }
+    }
+
+    private void ResetMatch()
+    {
+        PlayerOneScore = 0;
+        PlayerTwoScore = 0;
+        ball.SendMessage("RestartGame", null, SendMessageOptions.RequireReceiver);
+        restarting = false;
     }
 
     private void Update()
