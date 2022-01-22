92c076543a0cf735f4a4d4dcd9d5b0cdf44f71ba
WoodyEric <woodard.eric09@gmail.com>
2022-01-21 22:20:27 -0800

    add soundtrack and effects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,12 @@ public class GameManager : MonoBehaviour
 {
     public static int PlayerOneScore = 0;
     public static int PlayerTwoScore = 0;
-
+    private const int MAX_SCORE = 10;
     public Canvas helpScreen;
     public GUISkin layout;
 
     public GameObject ball;
+    public AudioSource audioSource;
 
     void Start()
     {
@@ -39,15 +40,19 @@ public class GameManager : MonoBehaviour
         GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerOneScore);
         GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerTwoScore);
 
-        if (PlayerOneScore == 10)
+        var messageRect = new Rect(Screen.width / 2 - 150, Screen.height - 100, 2000, 1000);
+        bool win = false;
+        if (PlayerOneScore >= MAX_SCORE)
         {
-            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
+            GUI.Label(messageRect, "PLAYER ONE WINS");
             ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+            win = true;
         }
-        else if (PlayerTwoScore == 10)
+        else if (PlayerTwoScore >= MAX_SCORE)
         {
-            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+            GUI.Label(messageRect, "PLAYER TWO WINS");
             ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+            win = true;
         }
     }
 
@@ -58,18 +63,23 @@ public class GameManager : MonoBehaviour
             Application.Quit();
         }
 
+        // pan the music to follow the ball
+        audioSource.panStereo = Mathf.Clamp(ball.transform.position.x / 4, - 0.5f, 0.5f);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (!helpScreen.gameObject.activeSelf)
             {
                 Time.timeScale = 0;
                 helpScreen.gameObject.SetActive(true);
+                audioSource.bypassEffects = false;
             }
             else
             {
 
                 Time.timeScale = 1;
                 helpScreen.gameObject.SetActive(false);
+                audioSource.bypassEffects = true;
             }
         }
     }
